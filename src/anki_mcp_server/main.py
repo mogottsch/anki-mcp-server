b4249b8bfbc192c62ed01a327120236256a2b4e5
Moritz Gottschling <gottschling.moritz@gmail.com>
2025-07-12 11:10:58 +0200

    Enable to configure host

--- src/anki_mcp_server/main.py
+++ src/anki_mcp_server/main.py
@@ -6,7 +6,9 @@ from anki_mcp_server.settings import get_settings
 
 def main():
     settings = get_settings()
-    mcp.run(transport=settings.mcp_transport, port=settings.mcp_port, host="*")
+    mcp.run(
+        transport=settings.mcp_transport, port=settings.mcp_port, host=settings.mcp_host
+    )
 
 
 if __name__ == "__main__":
--- src/anki_mcp_server/settings.py
+++ src/anki_mcp_server/settings.py
@@ -8,6 +8,7 @@ class Settings(BaseSettings):
     anki_connect_version: int = 6
     mcp_transport: Transport = "http"
     mcp_port: int = 8629
+    mcp_host: str = "localhost"
 
 
 @lru_cache()
